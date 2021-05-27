df369436140d9f2602764c8b90c78ef708c813b7
RaghadEstanbouli <Raghad.Estanbouli@flemingcollege.ca>
2021-05-27 14:21:37 -0400

    Update config.js

--- scrollymap_try/scrollfork/config.js
+++ scrollymap_try/scrollfork/config.js
@@ -1,30 +1,28 @@
-  
 // Number of slides that will drive (more = smoother)
 // If this doesn't match the number of slides named 'drive-slide' in config below you will not complete the full journey
-var driveSlides = 11;
+var driveSlides = 15;
 
 // Number of points on drive route (more = higher quality, but slower to process)
-var driveSmoothness = 190;
+var driveSmoothness = 180;
 
 // Value used to drive
 var driveTime = driveSlides*driveSmoothness;
 
 // Do you want to follow the point? True = follow
 // True is not that smpooth- working on seeing if can get it to follow more smoothly 
-var followPoint = true;
+var followPoint = false;
 // ...If so, what zoom, pitch, and bearing should be used to follow?
 var followZoomLevel = 7;
-var followBearing = 15;
+var followBearing = 19;
 var followPitch = 0;
 
 // to add 'driving' slides just make sure to add 'drive to beginning of slide id'
 // you also need to add a running total to the end of each 'drive-slide', (ex. drive-slide-0, drive-slide-1, drive-slide-2, etc.)
 var config = {
     style: 'mapbox://styles/mapbox/outdoors-v11',
-    accessToken: 'pk.eyJ1IjoicmFnaGFkZXN0YW5ib3VsaSIsImEiOiJja21vdmE1cWgyOHYxMnVtZ2EweTlzNW9rIn0.pH1YN5MUUxqb-py-3V6fcg',
+    accessToken: 'pk.eyJ1IjoiaG9semVnZyIsImEiOiJja292eXAzbnUwMHdhMnVsdTM4c2MyOTF3In0.6RiKfls_ZXfiI0k-q7gLDQ',
     showMarkers: true,
-    markerColor: '#2f4f4f',
-    theme: 'liberty',
+    theme: 'dark',
     alignment: 'left',
     title: 'Liberty Project',
     subtitle: 'A descriptive and interesting subtitle to draw in the reader',
@@ -39,11 +37,13 @@ var config = {
             image: '',
             description: 'The start Location for the Bike fundraiser',
             location: {
-                center: [-89.2477, 48.3809],
-                zoom: 9.83,
+                'camera': {
+                center: [-89.2477,  48.3809],
+                zoom: 12,
                 pitch: 0.00,
-                bearing: 0.00
+                bearing: 0.00}
             },
+            mapAnimation: 'flyTo',
             onChapterEnter: [],
             onChapterExit: []
         },
@@ -52,8 +52,14 @@ var config = {
             title: 'Fundraiser',
             // image: './path/to/image/source.png',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            location: { 
+            location: {
+                'camera': {
+                center: [-88.351609,  49.00186],
+                zoom: 12,
+                pitch: 0.00,
+                bearing: 0.00}
             },
+            mapAnimation: 'flyTo',
             onChapterEnter: [],
             onChapterExit: []
         },
@@ -63,8 +69,13 @@ var config = {
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                // location information is from the drive route
+                'camera': {
+                center: [-87.077195,  48.785168],
+                zoom: 12,
+                pitch: 0.00,
+                bearing: 0.00}
             },
+            mapAnimation: 'flyTo',
             onChapterEnter: [],
             onChapterExit: []
         },
@@ -73,7 +84,14 @@ var config = {
             title: 'Day 2???',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
-            location: {},
+            location: {
+                'camera': {
+                center: [-85.550095,  48.647444],
+                zoom: 12,
+                pitch: 0.00,
+                bearing: 0.00}
+            },
+            mapAnimation: 'flyTo',
             onChapterEnter: [],
             onChapterExit: []
         },
@@ -82,41 +100,47 @@ var config = {
             title: 'Second Title',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
-            location: {},
+            location: {
+                'camera': {
+                center: [-84.84697,  48.041381],
+                zoom: 12,
+                pitch: 0.00,
+                bearing: 0.00}
+            },
+            mapAnimation: 'flyTo',
             onChapterEnter: [],
             onChapterExit: []
         },
         {
             id: 'drive-slide-3',
-            title: 'Stop: WAWA',
+            title: 'Second Title',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: [ -84.84697, 48.04138],
-                zoom: 15.00,
-                pitch: 60.00,
-                bearing: 0.00 
+                'camera': {
+                center: [-84.748093,  47.472679],
+                zoom: 12,
+                pitch: 0.00,
+                bearing: 0.00}
             },
-            // mapAnimation: 'flyTo',
-            onChapterEnter: [ 
-                {
-                layer: 'Places',
-                opacity: 1
-                }
-            ],
-            onChapterExit: [
-                {
-                    layer: 'Places',
-                    opacity: 0
-                }
-            ]
+            mapAnimation: 'flyTo',
+      
+            onChapterEnter: [],
+            onChapterExit: []
         },
         {
             id: 'drive-slide-4',
             title: 'Second Title',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
-            location: {},
+            location: {
+                'camera': {
+                center: [-81.056687,  46.475716],
+                zoom: 12,
+                pitch: 0.00,
+                bearing: 0.00}
+            },
+            mapAnimation: 'flyTo',
             onChapterEnter: [],
             onChapterExit: []
         },
@@ -128,13 +152,15 @@ var config = {
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                // center: [-84.748093, 47.472679],
-                // zoom: 9.83,
-                // pitch: 0.00,
-                // bearing: 0.00 
+                'camera': {
+                center: [-78.980271,  44.535692],
+                zoom: 12,
+                pitch: 0.00,
+                bearing: 0.00}
             },
+            mapAnimation: 'flyTo',
             // mapAnimation: 'flyTo',
-            // rotateAnimation: false,
+            rotateAnimation: true,
             // callback: '',
             onChapterEnter: [],
             onChapterExit: []
@@ -144,7 +170,14 @@ var config = {
             title: 'Second Title',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
-            location: {},
+            location: {
+                'camera': {
+                center: [-79.738328,  45.17431],
+                zoom: 12,
+                pitch: 0.00,
+                bearing: 0.00}
+            },
+            mapAnimation: 'flyTo',
             onChapterEnter: [],
             onChapterExit: []
         },
@@ -153,7 +186,14 @@ var config = {
             title: 'Second Title',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
-            location: {},
+            location: {
+                'camera': {
+                center: [-80.424973,  45.656305],
+                zoom: 12,
+                pitch: 0.00,
+                bearing: 0.00}
+            },
+            mapAnimation: 'flyTo',
             onChapterEnter: [],
             onChapterExit: []
         },
@@ -162,7 +202,14 @@ var config = {
             title: 'Second Title',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
-            location: {},
+            location: {
+                'camera': {
+                center: [-81.776291,  46.259662],
+                zoom: 12,
+                pitch: 0.00,
+                bearing: 0.00}
+            },
+            mapAnimation: 'flyTo',
             onChapterEnter: [],
             onChapterExit: []
         },
@@ -171,7 +218,14 @@ var config = {
             title: 'Second Title',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
-            location: {},
+            location: {
+                'camera': {
+                center: [-82.951829,  46.183651],
+                zoom: 12,
+                pitch: 0.00,
+                bearing: 0.00}
+            },
+            mapAnimation: 'flyTo',
             onChapterEnter: [],
             onChapterExit: []
         },
@@ -180,35 +234,57 @@ var config = {
             title: 'Second Title',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
-            location: {},
+            location: {
+                'camera': {
+                center: [-83.781296,  46.320395],
+                zoom: 12,
+                pitch: 0.00,
+                bearing: 0.00}
+            },
+            mapAnimation: 'flyTo',
             onChapterEnter: [],
             onChapterExit: []
-        },
-        {
-            id: 'drive-slide-10',
-            title: 'Second Title',
-            image: './path/to/image/source.png',
-            description: 'Copy these sections to add to your story.',
-            location: {},
-            onChapterEnter: [ 
-                // {
-                // layer: 'stops',
-                // opacity: 1
-                // }
-            ],
-            onChapterExit: [
-                // {
-                //     layer: 'stops',
-                //     opacity: 0
-                // }
-            ]
         }
+        
+        // ,{
+        //     id: 'drive-slide-10',
+        //     title: 'Second Title',
+        //     image: './path/to/image/source.png',
+        //     description: 'Copy these sections to add to your story.',
+        //     location: {
+        //         'camera': {
+        //         center: [-89.2477,  48.3809],
+        //         zoom: 12,
+        //         pitch: 0.00,
+        //         bearing: 0.00}
+        //     },
+        //     mapAnimation: 'flyTo',
+        //     onChapterEnter: [ 
+        //         // {
+        //         // layer: 'stops',
+        //         // opacity: 1
+        //         // }
+        //     ],
+        //     onChapterExit: [
+        //         // {
+        //         //     layer: 'stops',
+        //         //     opacity: 0
+        //         // }
+        //     ]
+        // },
         // {
         //     id: 'drive-slide-11',
         //     title: 'Second Title',
         //     image: './path/to/image/source.png',
         //     description: 'Copy these sections to add to your story.',
-        //     location: {},
+        //     location: {
+        //         'camera': {
+        //         center: [-89.2477,  48.3809],
+        //         zoom: 12,
+        //         pitch: 0.00,
+        //         bearing: 0.00}
+        //     },
+        //     mapAnimation: 'flyTo',
         //     onChapterEnter: [],
         //     onChapterExit: []
         // },
@@ -217,7 +293,14 @@ var config = {
         //     title: 'Second Title',
         //     image: './path/to/image/source.png',
         //     description: 'Copy these sections to add to your story.',
-        //     location: {},
+        //     location: {
+        //         'camera': {
+        //         center: [-89.2477,  48.3809],
+        //         zoom: 12,
+        //         pitch: 0.00,
+        //         bearing: 0.00}
+        //     },
+        //     mapAnimation: 'flyTo',
         //     onChapterEnter: [],
         //     onChapterExit: []
         // },
@@ -226,7 +309,14 @@ var config = {
         //     title: 'Second Title',
         //     image: './path/to/image/source.png',
         //     description: 'Copy these sections to add to your story.',
-        //     location: {},
+        //     location: {
+        //         'camera': {
+        //         center: [-89.2477,  48.3809],
+        //         zoom: 12,
+        //         pitch: 0.00,
+        //         bearing: 0.00}
+        //     },
+        //     mapAnimation: 'flyTo',
         //     onChapterEnter: [],
         //     onChapterExit: []
         // },
@@ -235,7 +325,14 @@ var config = {
         //     title: 'Second Title',
         //     image: './path/to/image/source.png',
         //     description: 'Copy these sections to add to your story.',
-        //     location: {},
+        //     location: {
+        //         'camera': {
+        //         center: [-89.2477,  48.3809],
+        //         zoom: 12,
+        //         pitch: 0.00,
+        //         bearing: 0.00}
+        //     },
+        //     mapAnimation: 'flyTo',
         //     onChapterEnter: [],
         //     onChapterExit: []
         // }
