3ad4c612c307d0d0a18bba5a4f628eda3768d9a4
Raghad Estanbouli <Raghad.Estanbouli@flemingcollege.ca>
2021-05-28 10:58:20 -0400

    10:58

--- scrollymap_try/scrollfork/drive.js
+++ scrollymap_try/scrollfork/drive.js
@@ -25,7 +25,7 @@ function createLine() {
     const lineDistance = turf.lineDistance(line);
 
     // how many points you want along the path (more = smoother animation)
-    const rects = driveTime*0.9;
+    const rects = driveTime*0.8;
 
     // calculate the distance between each point on the path
     const segments = lineDistance / rects;
