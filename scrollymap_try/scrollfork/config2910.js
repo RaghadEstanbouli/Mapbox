3594075a20fe62562035e3790246e15901ebaa67
Raghad Estanbouli <Raghad.Estanbouli@flemingcollege.ca>
2021-05-29 22:53:27 -0400

    44

--- scrollymap_try/scrollfork/config2910.js
+++ scrollymap_try/scrollfork/config2910.js
@@ -1,3 +1,23 @@
+// Number of slides that will drive (more = smoother)
+// If this doesn't match the number of slides named 'drive-slide' in config below you will not complete the full journey
+var driveSlides = 14;
+
+// Number of points on drive route (more = higher quality, but slower to process)
+var driveSmoothness = 100;
+
+// Value used to drive
+var driveTime = driveSlides*driveSmoothness;
+
+// Do you want to follow the point? True = follow
+// True is not that smpooth- working on seeing if can get it to follow more smoothly 
+var followPoint = true;
+// ...If so, what zoom, pitch, and bearing should be used to follow?
+var followZoomLevel = 13;
+var followBearing = 40;
+var followPitch = 55.00;
+
+// to add 'driving' slides just make sure to add 'drive to beginning of slide id'
+// you also need to add a running total to the end of each 'drive-slide', (ex. drive-slide-0, drive-slide-1, drive-slide-2, etc.)
 var config = {
     style: 'mapbox://styles/vanritto/ckp6e61p50nwy17o58eyrsnoe',
     accessToken: 'pk.eyJ1IjoidmFucml0dG8iLCJhIjoiY2ttd3MwbmIxMGh2azJubjh5NHpmbWloaSJ9.cHakwI5tJ_9g4G56n5tlFQ',
@@ -18,10 +38,11 @@ var config = {
             image: '',
             description: 'The start Location for the Bike fundraiser',
             location: {
-                center: [-89.2477, 48.3809],
-                zoom: 18,
-                pitch: 0.00,
-                bearing: 0.00
+                
+                 center: [-89.21659, 48.43431]
+                // zoom: 9.83,
+                // pitch: 0.00,
+                // bearing: 0.00
             },
             onChapterEnter: [
                 {
@@ -89,26 +110,14 @@ var config = {
                 //     }
             ]
         },
-        // {
-        //     id: 'slide-0',
-        //     title: 'Fundraiser',
-        //     // image: './path/to/image/source.png',
-        //     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        //     location: { 
-        //     },
-        //     onChapterEnter: [],
-        //     onChapterExit: []
-        // },
         {
             id: 'drive-slide-0',
             title: 'STOP 1?',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
-             location: {
-                center: [-88.351609,49.00186],
-                zoom: 18,
-                pitch: 0.00,
-                bearing: 0.00
+            location: {
+                center: [-88.3516, 49.00186]
+                // location information is from the drive route
             },
             onChapterEnter: [
                 {
@@ -168,15 +177,12 @@ var config = {
             onChapterExit: []
         },
         {
-            id: 'drive-slide-01',
+            id: 'drive-slide-1',
             title: 'STOP 2',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: [-87.077195,48.785168],
-                zoom: 18,
-                pitch: 0.00,
-                bearing: 0.00
+                center: [ -87.07719, 48.78516]
             },
             onChapterEnter: [
                 {
@@ -236,14 +242,9 @@ var config = {
             title: 'Stop 3',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
-            
-            location: {
-                center: [-85.550095,48.647444],
-                zoom: 18,
-                pitch: 0.00,
-                bearing: 0.00
+            location: { 
+                center: [ -85.55009, 48.64744] 
             },
-            
             onChapterEnter: [
                 {
                     layer: 'stop-3',
@@ -299,12 +300,12 @@ var config = {
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: [-84.84697,48.041381],
-                zoom: 15.00,
-                pitch: 60.00,
-                bearing: 0.00 
+                 center: [ -84.84697, 48.04138],
+                 zoom: 15.00,
+                // pitch: 60.00,
+                // bearing: 0.00 
             },
-            // mapAnimation: 'flyTo',
+            mapAnimation: 'flyTo',
             onChapterEnter: [ 
                 {
                     layer: 'stop-4',
@@ -359,11 +360,8 @@ var config = {
             title: 'Stop 5',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
-             location: {
-                center: [-84.748093,47.472679],
-                zoom: 15.00,
-                pitch: 60.00,
-                bearing: 0.00 
+            location: {
+                center: [ -84.74809, 47.47267]
             },
             onChapterEnter: [
                 {
@@ -413,6 +411,7 @@ var config = {
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
+                center: [ -84.28666, 46.52109]
                 // center: [-84.748093, 47.472679],
                 // zoom: 9.83,
                 // pitch: 0.00,
@@ -429,7 +428,9 @@ var config = {
             title: 'STOP 6',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
-            location: {},
+            location: {
+                center: [ -84.28666, 46.52109]
+            },
             onChapterEnter: [
                 {
                     layer: 'stop-6',
@@ -472,10 +473,7 @@ var config = {
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: [-84.286667,46.521092],
-                zoom: 15.00,
-                pitch: 60.00,
-                bearing: 0.00 
+                center: [ -83.78129, 46.32039]
             },
             onChapterEnter: [
                 {
@@ -515,10 +513,7 @@ var config = {
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: [-83.781296,46.320395],
-                zoom: 15.00,
-                pitch: 60.00,
-                bearing: 0.00 
+                center: [ -82.95182, 46.18365]
             },
             onChapterEnter: [
                 {
@@ -553,11 +548,8 @@ var config = {
             title: 'Stop 9',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
-           location: {
-                center: [-82.951829,46.183651],
-                zoom: 15.00,
-                pitch: 60.00,
-                bearing: 0.00 
+            location: {
+                center: [ -81.77629, 46.25966]
             },
             onChapterEnter: [
                 {
@@ -589,10 +581,7 @@ var config = {
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: [-81.776291,46.259662],
-                zoom: 15.00,
-                pitch: 60.00,
-                bearing: 0.00 
+                center: [-81.05668, 46.47571]
             },
             onChapterEnter: [
                 {
@@ -615,15 +604,16 @@ var config = {
             onChapterExit: []
         },
         {
-            id: 'drive-slide-91',
+            id: 'drive-slide-10',
             title: 'Stop 11',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: [-81.056687,46.475716],
-                zoom: 15.00,
-                pitch: 60.00,
-                bearing: 0.00 
+                center: [-80.42497, 45.6563]
+                // center: [-78.31972, 44.30345],
+                // zoom: 15.00,
+                // pitch: 60.00,
+                // bearing: 0.00 
             },
             onChapterEnter: [ 
                 {
@@ -647,17 +637,13 @@ var config = {
             ]
         },
         {
-            id: 'drive-slide-92',
+            id: 'drive-slide-11',
             title: 'Stop 12',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: [-80.424973,45.656305],
-                zoom: 15.00,
-                pitch: 60.00,
-                bearing: 0.00 
+                center: [-79.73832, 45.17431]
             },
-            
             onChapterEnter: [
                 {
                 layer: 'stop-12',
@@ -671,15 +657,12 @@ var config = {
             onChapterExit: []
         },
         {
-            id: 'drive-slide-93',
+            id: 'drive-slide-12',
             title: 'Stop 13',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
-             location: {
-                center: [-79.738328,45.17431],
-                zoom: 15.00,
-                pitch: 60.00,
-                bearing: 0.00 
+            location: {
+                center: [-78.98027, 44.53569]
             },
             onChapterEnter: [
                 {
@@ -690,15 +673,12 @@ var config = {
             onChapterExit: []
         },
         {
-            id: 'drive-slide-94',
+            id: 'drive-slide-13',
             title: 'Stop 14',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: [-78.980271,44.535692],
-                zoom: 15.00,
-                pitch: 60.00,
-                bearing: 0.00 
+                center: [-78.31972, 44.30345]
             },
             onChapterEnter: [],
             onChapterExit: []
