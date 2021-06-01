ce9346446fdc70a04c507d096f338d8d4f840914
Raghad Estanbouli <Raghad.Estanbouli@flemingcollege.ca>
2021-06-01 14:20:23 -0400

    dd

--- scrollymap_try/scrollfork/config.js
+++ scrollymap_try/scrollfork/config.js
@@ -255,7 +255,6 @@ var config = {
         },
         {
             id: 'drive-slide-02',
-            hidden: true,
             title: 'STOP 1: Working in Mozambique.',
             image: './Images/TestImage2.png',
             description: 'The Liberty Project is a Christian faith-based non-profit established in Pemba, Northern Mozambique working with women who have been sexually exploited, abused and culturally oppressed. Our vision is to make disciples through sustainable initiatives lead by Local women. We are an organization empowering women to empower women.',
