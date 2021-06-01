4ce64099c9fb8724293253204f04a133f9fd49e2
Raghad Estanbouli <Raghad.Estanbouli@flemingcollege.ca>
2021-06-01 16:21:45 -0400

    dd

--- scrollymap_try/scrollfork/drive.js
+++ scrollymap_try/scrollfork/drive.js
@@ -35,7 +35,7 @@ function createLine() {
     // const segments = lineDistance / rects;
     // const segments = lineDistance / driveSlides / rects;
     // const segments = lineDistance / driveSlides;
-    const segments =  lineDistance / 1161;
+    const segments =  lineDistance / rects;
     console.log(" distance between each point on the path: "+ segments );
 
     // what units do you want to use?
