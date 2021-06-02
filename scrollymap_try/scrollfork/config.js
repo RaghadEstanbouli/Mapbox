35d0b10328c3777993cfdd55aadde2bf7e1cb793
Raghad Estanbouli <Raghad.Estanbouli@flemingcollege.ca>
2021-06-01 22:11:48 -0400

    f

--- scrollymap_try/scrollfork/config.js
+++ scrollymap_try/scrollfork/config.js
@@ -1395,6 +1395,307 @@ var config = {
             }
         ],
         onChapterExit: []
-    }         
+    }  ,
+    
+       //--------------------------------------------------------------------------to here
+    {
+        id: '32',
+        title: 'Stop 10: End Exploitation',
+        image: './Images/TestImage2.png',
+        description: '95% of women in Mozambique have been sexually abused; and 70% of women suffer from PTSD. This is not okay. Throughout the global shut down for COVID-19 we have seen continual increases of exploitation and trafficking world wide. We are here in Mozambique doing preventative work to empower and educate women to help break the cycles of exploitation and poverty.',
+        location: {
+            center: { lon: -81.04895, lat: 46.47691 },
+            zoom: 11.64,
+            pitch: 60.00,
+            bearing: -87.19
+        },
+        onChapterEnter: [
+            {
+            layer: 'stop-10',
+            opacity: 1
+            },
+            {
+            layer: 'stop-11',
+            opacity: 0
+            },
+            {
+            layer: 'stop-12',
+            opacity: 0
+            },
+            {
+            layer: 'stop-13',
+            opacity: 0
+            }
+        ],
+        onChapterExit: []
+    },
+    {
+        id: '33',
+        title: 'Stop 11: Bringing Hope',
+        image: './Images/TestImage2.png',
+        description: 'This ride has been such fun, even though it took a very different turn because of restrictions. Our Founder and Board Chair want to say a big thank you for everyone who donated, participated in the ride and shared our story with their friends & family!  We are excited to continue our journey in Northern Mozambique helping women to empower one another, & see the end of exploitation of women & children in Mozambique (& around the world).',
+        location: {
+            // center: { lon: -81.75867, lat: 46.25789 },
+            // zoom: 12.39,
+            // pitch: 60.00,
+            // bearing: -177.59
+            center: { lon: -80.44140, lat: 45.66449 },
+            zoom: 10.93,
+            pitch: 60.00,
+            bearing: -23.43
+        },
+        onChapterEnter: [ 
+            {
+            layer: 'stop-11',
+            opacity: 1
+            },
+            {
+            layer: 'stop-12',
+            opacity: 0
+            },
+            {
+            layer: 'stop-13',
+            opacity: 0
+            }
+        ],
+        onChapterExit: [
+            // {
+            //     layer: 'stops',
+            //     opacity: 0
+            // }
+        ]
+    },
+    {
+        id: '34',
+        title: 'Stop 12: Moving',
+        image: './Images/TestImage2.png',
+        description: 'WOMEN! Our ladies are wonderful and are the heart of the project. It is our absolute joy to journey with them as they start to experience true freedom when knowing their identity is found in Jesus’ love and purpose for them!   As our ministry shifts to an entirely different area in Northern Mozambique, please pray that we will continue to reach and support more women. We pray that God will provide us with the resources and strength to educate and empower the vulnerable through sharing the Gospel, teaching health and human rights and equipping women with new artisanal skills.',
+        location: {
+            // center: { lon: -81.04895, lat: 46.47691 },
+            // zoom: 11.64,
+            // pitch: 60.00,
+            // bearing: -87.19
+            center: { lon: -79.77637, lat: 45.16116 },
+            zoom: 9.93,
+            pitch: 60.00,
+            bearing: 0.00
+        },
+        
+        onChapterEnter: [
+            {
+            layer: 'stop-12',
+            opacity: 1
+            },
+            {
+            layer: 'stop-13',
+            opacity: 0
+            }
+        ],
+        onChapterExit: []
+    },
+    {
+        id: '35',
+        title: 'Stop 13: Partner with Us',
+        image: './Images/TestImage2.png',
+        description: 'Thank you for following our journey & for giving! This fundraiser will continue helping women be empowered, educated and to build a life free from exploitation.                                                       If you would like to participate in the virtual challenge please follow the link to our eventbrite, you will be emailed the link to the virtual race following your registration.',
+        location: {
+            // center: { lon: -80.44140, lat: 45.66449 },
+            // zoom: 10.93,
+            // pitch: 60.00,
+            // bearing: -23.43
+
+            center: { lon: -78.99838, lat: 44.55062 },
+            zoom: 9.29,
+            pitch: 60.00,
+            bearing: -21.03
+        },
+        onChapterEnter: [
+            {
+            layer: 'stop-13',
+            opacity: 1
+            }
+        ],
+        onChapterExit: []
+    },
+    {
+        id: '36',
+        title: 'Stop 14: Working in Mozambique.',
+        image: './Images/TestImage2.png',
+        description: 'TEST TEXT.  The Liberty Project is a Christian faith-based non-profit established in Pemba, Northern Mozambique working with women who have been sexually exploited, abused and culturally oppressed. Our vision is to make disciples through sustainable initiatives lead by Local women. We are an organization empowering women to empower women.',
+        location:  {
+            // center: { lon: -79.77637, lat: 45.16116 },
+            // zoom: 9.93,
+            // pitch: 60.00,
+            // bearing: 0.00
+            center: { lon: -78.34254, lat: 44.32538 },
+            zoom: 9.79,
+            pitch: 60.00,
+            bearing: -41.03
+        },
+        onChapterEnter: [
+            {
+                }
+        ],
+        onChapterExit: []
+    },
+    {
+        id: '37',
+        title: 'Stop 10: End Exploitation',
+        image: './Images/TestImage2.png',
+        description: '95% of women in Mozambique have been sexually abused; and 70% of women suffer from PTSD. This is not okay. Throughout the global shut down for COVID-19 we have seen continual increases of exploitation and trafficking world wide. We are here in Mozambique doing preventative work to empower and educate women to help break the cycles of exploitation and poverty.',
+        location: {
+            center: { lon: -81.04895, lat: 46.47691 },
+            zoom: 11.64,
+            pitch: 60.00,
+            bearing: -87.19
+        },
+        onChapterEnter: [
+            {
+            layer: 'stop-10',
+            opacity: 1
+            },
+            {
+            layer: 'stop-11',
+            opacity: 0
+            },
+            {
+            layer: 'stop-12',
+            opacity: 0
+            },
+            {
+            layer: 'stop-13',
+            opacity: 0
+            }
+        ],
+        onChapterExit: []
+    },
+    {
+        id: '38',
+        title: 'Stop 11: Bringing Hope',
+        image: './Images/TestImage2.png',
+        description: 'This ride has been such fun, even though it took a very different turn because of restrictions. Our Founder and Board Chair want to say a big thank you for everyone who donated, participated in the ride and shared our story with their friends & family!  We are excited to continue our journey in Northern Mozambique helping women to empower one another, & see the end of exploitation of women & children in Mozambique (& around the world).',
+        location: {
+            // center: { lon: -81.75867, lat: 46.25789 },
+            // zoom: 12.39,
+            // pitch: 60.00,
+            // bearing: -177.59
+            center: { lon: -80.44140, lat: 45.66449 },
+            zoom: 10.93,
+            pitch: 60.00,
+            bearing: -23.43
+        },
+        onChapterEnter: [ 
+            {
+            layer: 'stop-11',
+            opacity: 1
+            },
+            {
+            layer: 'stop-12',
+            opacity: 0
+            },
+            {
+            layer: 'stop-13',
+            opacity: 0
+            }
+        ],
+        onChapterExit: [
+            // {
+            //     layer: 'stops',
+            //     opacity: 0
+            // }
+        ]
+    },
+    {
+        id: '39',
+        title: 'Stop 12: Moving',
+        image: './Images/TestImage2.png',
+        description: 'WOMEN! Our ladies are wonderful and are the heart of the project. It is our absolute joy to journey with them as they start to experience true freedom when knowing their identity is found in Jesus’ love and purpose for them!   As our ministry shifts to an entirely different area in Northern Mozambique, please pray that we will continue to reach and support more women. We pray that God will provide us with the resources and strength to educate and empower the vulnerable through sharing the Gospel, teaching health and human rights and equipping women with new artisanal skills.',
+        location: {
+            // center: { lon: -81.04895, lat: 46.47691 },
+            // zoom: 11.64,
+            // pitch: 60.00,
+            // bearing: -87.19
+            center: { lon: -79.77637, lat: 45.16116 },
+            zoom: 9.93,
+            pitch: 60.00,
+            bearing: 0.00
+        },
+        
+        onChapterEnter: [
+            {
+            layer: 'stop-12',
+            opacity: 1
+            },
+            {
+            layer: 'stop-13',
+            opacity: 0
+            }
+        ],
+        onChapterExit: []
+    },
+    {
+        id: '40',
+        title: 'Stop 11: Bringing Hope',
+        image: './Images/TestImage2.png',
+        description: 'This ride has been such fun, even though it took a very different turn because of restrictions. Our Founder and Board Chair want to say a big thank you for everyone who donated, participated in the ride and shared our story with their friends & family!  We are excited to continue our journey in Northern Mozambique helping women to empower one another, & see the end of exploitation of women & children in Mozambique (& around the world).',
+        location: {
+            // center: { lon: -81.75867, lat: 46.25789 },
+            // zoom: 12.39,
+            // pitch: 60.00,
+            // bearing: -177.59
+            center: { lon: -80.44140, lat: 45.66449 },
+            zoom: 10.93,
+            pitch: 60.00,
+            bearing: -23.43
+        },
+        onChapterEnter: [ 
+            {
+            layer: 'stop-11',
+            opacity: 1
+            },
+            {
+            layer: 'stop-12',
+            opacity: 0
+            },
+            {
+            layer: 'stop-13',
+            opacity: 0
+            }
+        ],
+        onChapterExit: [
+            // {
+            //     layer: 'stops',
+            //     opacity: 0
+            // }
+        ]
+    },
+    {
+        id: '41',
+        title: 'Stop 12: Moving',
+        image: './Images/TestImage2.png',
+        description: 'WOMEN! Our ladies are wonderful and are the heart of the project. It is our absolute joy to journey with them as they start to experience true freedom when knowing their identity is found in Jesus’ love and purpose for them!   As our ministry shifts to an entirely different area in Northern Mozambique, please pray that we will continue to reach and support more women. We pray that God will provide us with the resources and strength to educate and empower the vulnerable through sharing the Gospel, teaching health and human rights and equipping women with new artisanal skills.',
+        location: {
+            // center: { lon: -81.04895, lat: 46.47691 },
+            // zoom: 11.64,
+            // pitch: 60.00,
+            // bearing: -87.19
+            center: { lon: -79.77637, lat: 45.16116 },
+            zoom: 9.93,
+            pitch: 60.00,
+            bearing: 0.00
+        },
+        
+        onChapterEnter: [
+            {
+            layer: 'stop-12',
+            opacity: 1
+            },
+            {
+            layer: 'stop-13',
+            opacity: 0
+            }
+        ],
+        onChapterExit: []
+    }             
     ]
 };
