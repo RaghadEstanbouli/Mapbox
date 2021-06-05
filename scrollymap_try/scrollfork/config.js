48862d2eb58e2e8bfdef15a1ef935072b8434ff8
RaghadEstanbouli <Raghad.Estanbouli@flemingcollege.ca>
2021-06-05 13:45:58 -0400

    Update config.js

--- scrollymap_try/scrollfork/config.js
+++ scrollymap_try/scrollfork/config.js
@@ -3,7 +3,7 @@
 var driveSlides = 20;
 
 // Number of points on drive route (more = higher quality, but slower to process)
-var driveSmoothness = 200;
+var driveSmoothness = 20;
 
 // Value used to drive
 var driveTime = driveSlides*driveSmoothness;
